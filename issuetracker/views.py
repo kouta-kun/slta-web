c2ea87f4f07a1a5916c76ae96c24427c00a08177
Kouta-kun <darkfm@vera.com.uy>
2019-11-09 12:42:54 -0300

    la concha negra de la lora

--- issuetracker/views.py
+++ issuetracker/views.py
@@ -1,12 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from django.template import loader
 from . import models
 
 
 # Create your views here.
-def index(httpreq):
-    temp = loader.get_template("issuetracker/index.html")
-    iss = models.Issue.objects.select_related('reporter').all()
-    ctx = {"issues": iss}
-    return HttpResponse(temp.render(ctx, httpreq))
+def index(httpreq: HttpRequest):
+    if httpreq.method == 'GET':
+        temp = loader.get_template("issuetracker/index.html")
+        iss = models.Issue.objects.select_related('reporter').all()
+        ctx = {"issues": iss}
+        return HttpResponse(temp.render(ctx, httpreq))
+    elif httpreq.method == 'POST':
+        user = httpreq.POST['user']
+        user = models.User.objects.get(name=user)
+        pin = httpreq.POST['pin']
+        if user.pin != pin:
+            return HttpResponse("PIN incorrecto", status=403)
+        message = httpreq.POST['desc']
+        models.Issue(reporter=user, description=message).save()
