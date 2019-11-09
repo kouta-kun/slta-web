81e85403fb74d5900d9a27741cf23fde36f898c9
unknown <darkfm@vera.com.uy>
2019-11-09 13:41:42 -0300

    fix

--- issuetracker/models.py
+++ issuetracker/models.py
@@ -8,15 +8,15 @@ class User(models.Model):
 
 
 class Update(models.Model):
-    codigo_mayor = models.SmallIntegerField(primary_key=True)
-    codigo_menor = models.SmallIntegerField(primary_key=True)
+    codigo_version = models.CharField(max_length=4, primary_key=True)
     fecha_release = models.DateField()
     def nombre_update(self):
-        return f"{self.codigo_mayor}.{self.codigo_menor}"
+        return f"{self.codigo_version} ({self.fecha_release})"
 
 class Issue(models.Model):
+    issue_id = models.AutoField(primary_key=True)
     reporter = models.ForeignKey(User, primary_key=False, on_delete=models.CASCADE)
-    description = models.TextField(primary_key=True)
+    description = models.TextField(primary_key=False)
     status = models.CharField(choices=[("P", "Progreso"), ("R", "Reparado")], default="P", max_length=1)
     report_time = models.DateField(auto_now_add=True)
-    fixed_in = models.ForeignKey(Update, primary_key=False, null=True, default=None)
+    fixed_in = models.ForeignKey(Update, primary_key=False, null=True, default=None, on_delete=models.CASCADE)
